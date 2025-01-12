2c4996bef647c83df9446bd1fb83c011ae329750
itizawa <itizawa.pen@gmail.com>
2025-01-12 14:56:59 +0900

    Change: button の表示

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,21 @@
+import Button from "@/components/Buttons";
+import ImageViewer from "@/components/ImageViewer";
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
+const PlaceholderImage = require("@/assets/images/background-image.png");
+
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button theme="secondary" label="Use this photo" />
+      </View>
       <Link href="/about" style={styles.button}>
         Go to About screen
       </Link>
@@ -14,19 +23,18 @@ export default function Index() {
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    backgroundColor: "#111",
     alignItems: "center",
-    backgroundColor: "#111"
   },
   text: {
     color: "#eee",
     fontSize: 20,
   },
-  button:{
+  button: {
     backgroundColor: "#007bff",
     padding: 10,
     borderRadius: 5,
@@ -35,5 +43,13 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 16,
     textAlign: "center",
-  }
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
 });
